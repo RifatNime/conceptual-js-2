14343dfbec5f1752d2cf07806ca20bd33f3aa777
RifatNime <mdrifatnime@gmail.com>
2022-02-01 00:58:28 +0600

    tonight

---
+++ object-details.js
@@ -0,0 +1,14 @@
+// Js Objects details
+var person = {
+    name: "Rifat",//key value pair
+    age: 32,
+    height: "5 feet 7 inch",
+    hairColor: 'Black',
+    gender: "Men",
+    male: true,
+    cardNum: [12345674, 23654789, 12359864],
+}
+// var arr = ['opuu', 'Mamun'];
+console.log(person.cardNum);
+console.log(person.cardNum[2]);
+console.log(person);
