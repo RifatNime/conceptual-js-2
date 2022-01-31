b2f285551a6ceb9a425c7f66890ef2389f6f444d
RifatNime <mdrifatnime@gmail.com>
2022-01-31 23:54:41 +0600

    first commit

---
+++ function-practise.js
@@ -0,0 +1,11 @@
+// function sumOfNumbers(num1, num2){
+//     var result = num1 + num2;
+//     return result;
+// }
+// console.log(sumOfNumbers(65, 4));
+//
+function mesgGenerator(name){
+    var message = "Hello " + name + ", Welcome to programmimg hero.";
+    return message;
+}
+console.log(mesgGenerator("Md Rifat Nime"));
