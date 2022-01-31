17f76c9ba5263109dd9ac3c31912bb799f706fb4
RifatNime <mdrifatnime@gmail.com>
2022-02-01 00:36:04 +0600

    function

--- food-delivary.js
+++ food-delivary.js
@@ -4,9 +4,15 @@ function pizzaPanda(foodName, quantity){
     if(foodName.toLowerCase() != 'pizza'){
         return 'Sorry, We sell pizza only.';
     }
+    else if(quantity => 10){
+        return 'You can order, But You have to wait.'
+    }
+    else if (quantity <= 0){
+        return 'Sorry, Use positive number.'
+    }
 
     var price = 10 * quantity;
-    var message = "Order Is successfull!!, \nOrder Info: \nFood Name: " + foodName + ",\nPrice: "+ price + '$'; 
+    var message = "Your order Is successfull!!, \nOrder Info: \nFood Name: " + foodName + ",\nPrice: "+ price + '$'; 
     return message;
 }
-console.log(pizzaPanda("milk", 3));
+console.log(pizzaPanda("pizza", 11));
